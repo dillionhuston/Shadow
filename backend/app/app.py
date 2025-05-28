a2cb2c1be3908a41b086cd710a080874606b5837
Dillon Huston <122535848+dillionhuston@users.noreply.github.com>
2025-05-28 13:14:32 +0100

    Update app.py

--- backend/app/app.py
+++ backend/app/app.py
@@ -252,4 +252,5 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000 )
+    app.run(debug=True, host='0.0.0.0', port=5000 ) # change to config, so user can choose port/host
+    
